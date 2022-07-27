4b88c0ad640b6df27cb95120b45e0d1d012f0960
Hayato-7812 <63275973+Hayato-7812@users.noreply.github.com>
2022-07-28 01:23:10 +0900

    追加、削除機能実装

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  //textBoxの値を取得し初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  //divタグの生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  //liタグの生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  //buttoon(完了)の生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  //buttoon(削除)の生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    //押された削除ボタンの親要素(div)を削除する
+    const deleteTarget = deleteButton.parentNode;
+    //子要素からの指定を行う時はremoveChild()
+    document.getElementById("incomplete-list").removeChild(deleteTarget);
+  });
+
+  //divタグの子要素に各要素を設定
+  div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+  // console.log(div);
+
+  //未完了のリストに追加
+  document.getElementById("incomplete-list").appendChild(div);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
